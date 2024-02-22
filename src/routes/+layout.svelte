20c15509da66b490ceeec5fbacb3221000f7bc13
sedleri <richard.sedlecky@seznam.cz>
2024-02-22 22:32:49 +0100

    Feat: Add Theming

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,17 @@
 <script>
 	import '../app.css';
-	import Navbar from '../components/Navbar.svelte';
+	import Navbar from '../components/Navbar.svelte';	
+	import { ModeWatcher } from "mode-watcher";	
+
+	import { Button } from "$lib/components/ui/button";
+ 	import Sun from "lucide-svelte/icons/sun";
+  	import Moon from "lucide-svelte/icons/moon";
+ 
+  	import { toggleMode } from "mode-watcher";
 </script>
 
 <main class="p-2">
 	<Navbar />
+	<ModeWatcher />
 	<slot />
 </main>
